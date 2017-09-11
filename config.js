4b030abb9b370569cc5b2c63f3e0d4fe15ab2531
Rnso <rnso.pnr@gmail.com>
2017-09-11 20:39:46 +0400

    initial commit

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ export const nodeEnv = env.NODE_ENV || 'development'
 
 
 export default {
-    mongodbUri : `mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@ds129434.mlab.com:29434/fcc-corkboard`,
-    //mongodbUri : 'mongodb://localhost:27017/pintrestClone',
+    
+    mongodbUri : 'mongodb://localhost:27017/pintrestClone',
     port: env.PORT || 9000,
     host: env.HOST || '0.0.0.0'
 }
